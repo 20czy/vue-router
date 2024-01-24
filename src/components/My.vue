30a70ab5c32d64a6a371065b2bcda89618232043
20czy <3092492683@qq.com>
2024-01-24 11:18:57 +0800

    实现了路由的嵌套

--- src/components/My.vue
+++ src/components/My.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
         <h1>我的音乐</h1>
+        <router-link to="/my/1">商品1</router-link>
+        <router-link to="/my/2">商品2</router-link>
+        <router-link to="/my/3">商品3</router-link>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,26 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Discover from '../components/Discover.vue'
 import Friends from '../components/Friends.vue'
 import My from '../components/My.vue'
-
+import Toplist from '../components/Recomond.vue'
+import Songlist from '../components/Songlist.vue'
+import Product from '../components/Product.vue'
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
-        {path:'/discover', component:Discover},
+        {path: '/', redirect:"/discover"},
+        {path:'/discover', component:Discover,
+            children: [
+                {path:"toplist",component:Toplist},
+                {path:"playlist",component:Songlist},
+            ]
+    },    
         {path:'/friends', component:Friends},
-        {path:'/my', component:My},
+        {path:'/my', component:My,
+            children: [
+                {path:":id",component:Product, props:true},
+            ]
+    },
     ]
 })
 
